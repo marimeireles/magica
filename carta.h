353a3930e80338d41c0a0681b3d9b4337b961734
Kristiankab <42365389+Kristiankab@users.noreply.github.com>
2018-11-18 14:57:50 -0200

    Update carta.h
    Escopo de carta.h

--- carta.h
+++ carta.h
@@ -2,23 +2,23 @@
 
 #include "define.h"
 
-#ifndef CARD_H
-#define CARD_H
-class Card {
+#ifndef CARTA_H
+#define CARTA_H
+class Carta {
 private:
 	const char* nome; // Nome da carta.
-	cartaTipo type; // Tipo da carta, floresta, ilha, cemitério ou montanha.
+	cartaTipo tipo; // Tipo da carta, floresta, ilha, cemitério ou montanha.
 	int ataque; // Ataque da Carta
 	int defesa; // Defesa da Carta
 public:
-	Card();
-	Card(char*, cartaTipo);
-	Card(char*, cartaTipo, int, int);
+	Carta();
+	Carta(char*, cartaTipo);
+	Carta(char*, cartaTipo, int, int);
 	const char* getNome(); // Nome da carta
 	cartaTipo getTipo();	 // Tipo da carta
 	int getAtaque(); // Ataque da carta
 	int getDefesa(); // Defesa da carta
-	const void setName(const char*); // Nome da carta
+	const void setNome(const char*); // Nome da carta
 	void setTipo(cardTipo);	// Tipo da carta
 	void setAtaque(int); // Ataque da carta
 	void setDefesa(int); // Defesa da carta
